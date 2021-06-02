031dac558d5da63dc02a2c36f59ea61a5dd4c7fd
guoyukai <643582433@qq.com>
2021-06-02 11:10:48 +0800

    SpringMvc#nacos配置中心#文件加载顺序测试

--- lab-44/lab-44-nacos-config-demo-actuator/src/main/java/cn/iocoder/springboot/lab44/nacosdemo/Application.java
+++ lab-44/lab-44-nacos-config-demo-actuator/src/main/java/cn/iocoder/springboot/lab44/nacosdemo/Application.java
@@ -2,7 +2,6 @@ package cn.iocoder.springboot.lab44.nacosdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 // @NacosPropertySource(dataId = "example", type = ConfigType.YAML)
 public class Application {
