7fe9216ae07e76ad09eb85003b4cb29b0044f5f9
guoyukai <643582433@qq.com>
2021-07-06 18:26:51 +0800

    接口管理平台测试#已完成

--- lab-24/lab-24-apidoc-japidocs/src/main/java/cn/iocoder/springboot/lab24/TestJApiDocs.java
+++ lab-24/lab-24-apidoc-japidocs/src/main/java/cn/iocoder/springboot/lab24/TestJApiDocs.java
@@ -9,8 +9,8 @@ public class TestJApiDocs {
     public static void main(String[] args) {
         // 1. 创建生成文档的配置
         DocsConfig config = new DocsConfig();
-        config.setProjectPath("/Users/yunai/Java/SpringBoot-Labs/lab-24/lab-24-apidoc-japidocs"); // 项目所在目录
-        config.setDocsPath("/Users/yunai/Downloads/"); // 生成 HTML 接口文档的目标目录
+        config.setProjectPath("C:/MyData/Project/SpringBoot-Labs/lab-24/lab-24-apidoc-japidocs"); // 项目所在目录
+        config.setDocsPath("C:/Users/fw001/Desktop"); // 生成 HTML 接口文档的目标目录
         config.setAutoGenerate(true); // 是否给所有 Controller 生成接口文档
         config.setProjectName("示例项目"); // 项目名
         config.setApiVersion("V1.0"); // API 版本号
