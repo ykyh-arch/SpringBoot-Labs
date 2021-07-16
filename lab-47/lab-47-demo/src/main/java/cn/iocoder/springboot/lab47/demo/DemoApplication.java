6f299b8e6328deb28a7668770c06280366948f48
guoyukai <643582433@qq.com>
2021-07-16 18:43:01 +0800

    spring boot 自动配置原理

--- lab-47/lab-47-demo/src/main/java/cn/iocoder/springboot/lab47/demo/DemoApplication.java
+++ lab-47/lab-47-demo/src/main/java/cn/iocoder/springboot/lab47/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package cn.iocoder.springboot.lab47.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.autoconfigure.web.embedded.EmbeddedWebServerFactoryCustomizerAutoConfiguration;
 
 @SpringBootApplication
 public class DemoApplication {
