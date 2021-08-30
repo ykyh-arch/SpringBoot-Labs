b5e8fb71e73ecb5f3dd5e59a75271b3552938ddd
guoyukai <643582433@qq.com>
2021-08-30 10:26:19 +0800

    springboot mapstruct 入门测试#已完成

--- lab-55/lab-55-mapstruct-demo/src/main/java/cn/iocoder/springboot/lab55/mapstructdemo/convert/UserConvert.java
+++ lab-55/lab-55-mapstruct-demo/src/main/java/cn/iocoder/springboot/lab55/mapstructdemo/convert/UserConvert.java
@@ -8,9 +8,11 @@ import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
+// 注解映射器
 @Mapper
 public interface UserConvert {
 
+    // 通过调用 Mappers 的 #getMapper(Class<T> clazz) 方法，获得 MapStruct 帮我们自动生成的 UserConvert 实现类的对象。
     UserConvert INSTANCE = Mappers.getMapper(UserConvert.class);
 
     UserBO convert(UserDO userDO);
