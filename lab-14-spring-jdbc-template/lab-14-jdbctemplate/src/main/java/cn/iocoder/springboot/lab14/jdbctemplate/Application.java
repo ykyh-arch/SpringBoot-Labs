ed3fac7d6087059a89c2389094810e5923401039
guoyukai <643582433@qq.com>
2021-09-22 18:07:41 +0800

    springboot jdbcTemplate 测试#已完成

--- lab-14-spring-jdbc-template/lab-14-jdbctemplate/src/main/java/cn/iocoder/springboot/lab14/jdbctemplate/Application.java
+++ lab-14-spring-jdbc-template/lab-14-jdbctemplate/src/main/java/cn/iocoder/springboot/lab14/jdbctemplate/Application.java
@@ -2,6 +2,14 @@ package cn.iocoder.springboot.lab14.jdbctemplate;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * JdbcTemplate 演示测试，使用的 JDBC 一般来说，一共有 3 种方式：
+ * 原生 JDBC ，或者自己项目封装的 JDBC 工具类。
+ * Apache Common ，提供了 DbUtils 工具类。
+ * Spring JDBC ，提供了 JdbcTemplate 工具类。推荐使用这种
+ * @author Jaquez
+ * @date 2021/09/22 17:23
+ */
 @SpringBootApplication
 public class Application {
 }
