db6b071805c8de707cfc7af3eb96169d5d3c44f2
guoyukai <643582433@qq.com>
2021-09-25 16:54:44 +0800

    springboot security 入门测试#已完成

--- lab-01-spring-security/lab-01-springsecurity-demo/src/main/java/cn/iocoder/springboot/lab01/springsecurity/Application.java
+++ lab-01-spring-security/lab-01-springsecurity-demo/src/main/java/cn/iocoder/springboot/lab01/springsecurity/Application.java
@@ -2,7 +2,14 @@ package cn.iocoder.springboot.lab01.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.security.web.authentication.ui.DefaultLoginPageGeneratingFilter;
 
+/**
+ * Spring Security 入门测试，默认情况下，{@link UserDetailsServiceAutoConfiguration } 自动化配置类，会创建一个内存级别的 InMemoryUserDetailsManager Bean 对象，提供认证的用户信息。
+ * @author Jaquez
+ * @date 2021/09/25 16:41
+ */
 @SpringBootApplication
 public class Application {
 
--- lab-01-spring-security/lab-01-springsecurity-demo/src/main/java/cn/iocoder/springboot/lab01/springsecurity/controller/AdminController.java
+++ lab-01-spring-security/lab-01-springsecurity-demo/src/main/java/cn/iocoder/springboot/lab01/springsecurity/controller/AdminController.java
@@ -4,6 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 示例测试
+ * @author Jaquez
+ * @date 2021/09/25 16:51
+ */
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
