281dd185f31563cebafa67755827399470f95df2
guoyukai <643582433@qq.com>
2021-11-22 15:21:50 +0800

    springboot log#测试

--- lab-37/lab-37-logging-aop/src/main/java/cn/iocoder/springboot/lab37/loggingdemo/controller/DemoController.java
+++ lab-37/lab-37-logging-aop/src/main/java/cn/iocoder/springboot/lab37/loggingdemo/controller/DemoController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 记录访问日志控制器示例
+ *
+ * @author Jaquez
+ * @date 2021/11/22 15:06
+ */
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
---
+++ lab-73-aop/《芋道 Spring Boot aop参考》.md
@@ -0,0 +1,1 @@
+<https://www.iocoder.cn/Fight/Thoroughly-conquer-the-theory-of-Spring-AOP/?self>
