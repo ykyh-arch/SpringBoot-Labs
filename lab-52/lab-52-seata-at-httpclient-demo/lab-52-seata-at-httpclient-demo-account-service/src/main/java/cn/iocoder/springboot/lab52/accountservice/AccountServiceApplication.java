b920e16a2276951b186ddb29bfbe06cc09d11347
guoyukai <643582433@qq.com>
2022-06-10 14:25:02 +0800

    springboot + httpclient 多数据源事务测试

--- lab-52/lab-52-seata-at-httpclient-demo/lab-52-seata-at-httpclient-demo-account-service/src/main/java/cn/iocoder/springboot/lab52/accountservice/AccountServiceApplication.java
+++ lab-52/lab-52-seata-at-httpclient-demo/lab-52-seata-at-httpclient-demo-account-service/src/main/java/cn/iocoder/springboot/lab52/accountservice/AccountServiceApplication.java
@@ -3,6 +3,12 @@ package cn.iocoder.springboot.lab52.accountservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ *
+ * @author Jaquez
+ * @date 2022/06/10 11:51
+ */
 @SpringBootApplication
 public class AccountServiceApplication {
 
--- lab-52/lab-52-seata-at-httpclient-demo/lab-52-seata-at-httpclient-demo-product-service/src/main/java/cn/iocoder/springboot/lab52/productservice/ProductServiceApplication.java
+++ lab-52/lab-52-seata-at-httpclient-demo/lab-52-seata-at-httpclient-demo-product-service/src/main/java/cn/iocoder/springboot/lab52/productservice/ProductServiceApplication.java
@@ -3,6 +3,12 @@ package cn.iocoder.springboot.lab52.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ *
+ * @author Jaquez
+ * @date 2022/06/10 11:39
+ */
 @SpringBootApplication
 public class ProductServiceApplication {
 
