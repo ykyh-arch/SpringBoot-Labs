843cddb6e650f49a2825f4ee81f632b0e4475d5d
guoyukai <643582433@qq.com>
2022-06-12 17:43:02 +0800

    性能测试-tomcat、jetty、undertow 测试

--- lab-05-benchmark-tomcat-jetty-undertow/lab-05-tomcat/src/main/java/cn/iocoder/springboot/labs/lab05/tomcat/TomcatApplication.java
+++ lab-05-benchmark-tomcat-jetty-undertow/lab-05-tomcat/src/main/java/cn/iocoder/springboot/labs/lab05/tomcat/TomcatApplication.java
@@ -2,9 +2,14 @@ package cn.iocoder.springboot.labs.lab05.tomcat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sun.tools.jar.resources.jar;
 
 /**
  * 启动类，测试参数：java -jar lab-05-tomcat-1.0-SNAPSHOT.jar -Xms2g -Xmx2g -Xmn1g -XX:MaxMetaspaceSize=256m -Xss256k
+ *
+ * 容器优化考虑点：线程数、超时时间、jvm 优化
+ *
+ * springboot 项目优化参考：https://mp.weixin.qq.com/s/TpcB9dsMv9Y6BL1lwANo4Q,https://mp.weixin.qq.com/s/RkaJD6RTHBPUzTx5HAT_NQ
  *
  * @author Jaquez
  * @date 2022/06/12 11:08
@@ -15,5 +20,26 @@ public class TomcatApplication {
     public static void main(String[] args) {
         SpringApplication.run(TomcatApplication.class);
     }
+
+    /*远程调试，使用 IDEA 远程调试*/
+    /**
+     *
+     * java -Djavax.net.debug=
+     *     ssl -Xdebug -Xnoagent -Djava.compiler=
+     *     NONE -Xrunjdwp:transport=
+     *     dt_socket,server=y,suspend=
+     *     n,address=8888 -
+     *     jar springboot-1.0.jar
+     *
+     */
+    /*远程调试，使用 jvm 工具远程调试*/
+    /**
+     *
+     * java -jar -Djava.rmi.server.hostname=192.168.44.128 -
+     * Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=911 -
+     * Dcom.sun.management.jmxremote.ssl=false -
+     * Dcom.sun.management.jmxremote.authenticate=false jantent-1.0-SNAPSHOT.jar
+     *
+     */
 
 }
---
+++ lab-05-benchmark-tomcat-jetty-undertow/lab-05-tomcat/src/main/java/cn/iocoder/springboot/labs/lab05/tomcat/config/TomcatConfig.java
@@ -0,0 +1,40 @@
+package cn.iocoder.springboot.labs.lab05.tomcat.config;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * TomcatConfig 配置类
+ *
+ * @author jaquez
+ * @date 2022/06/12 17:07
+ **/
+@Configuration
+public class TomcatConfig {
+
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory tomcatFactory = new TomcatServletWebServerFactory();
+        tomcatFactory.addConnectorCustomizers(new MyTomcatConnectorCustomizer());
+        tomcatFactory.setPort(8005); // 端口
+        tomcatFactory.setContextPath("/tomcat-demo"); // 上下文路径
+        return tomcatFactory;
+    }
+    class MyTomcatConnectorCustomizer implements TomcatConnectorCustomizer {
+        public void customize(Connector connector) {
+            Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+            // 设置最大连接数，参考 Acceptor 类
+            protocol.setMaxConnections(10000);
+            // 设置最大线程数
+            protocol.setMaxThreads(200);
+            // 连接超时
+            protocol.setConnectionTimeout(5000);
+        }
+    }
+
+}
