edeefee53a3d71e38f550b3d206e12a1895bf798
guoyukai <643582433@qq.com>
2022-06-27 14:34:11 +0800

    arthas 测试

--- lab-41/lab-41-demo02/src/main/java/cn/iocoder/springboot/lab40/jenkinsdemo/controller/DemoController.java
+++ lab-41/lab-41-demo02/src/main/java/cn/iocoder/springboot/lab40/jenkinsdemo/controller/DemoController.java
@@ -4,6 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Spring Boot应用的后台运行配置参考：https://blog.didispace.com/spring-boot-run-backend/
+ *
+ * @author Jaquez
+ * @date 2022/06/27 10:09
+ */
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
---
+++ lab-81-arthas/《芋道 arthas 诊断工具》.md
@@ -0,0 +1,4 @@
+<https://mp.weixin.qq.com/s?__biz=MzU1Nzg4NjgyMw==&mid=2247485726&idx=1&sn=ebb7d66e3c1f7c5c75c6970ecd1f6f5f&scene=21#wechat_redirect>
+<https://mp.weixin.qq.com/s/Fc2WZHhr6iuAbd3kFG8hSg>
+官方地址参考
+<https://arthas.aliyun.com/doc/docker.html>
