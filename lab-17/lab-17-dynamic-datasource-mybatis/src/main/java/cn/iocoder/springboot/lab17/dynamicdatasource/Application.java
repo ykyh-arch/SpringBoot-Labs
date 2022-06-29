d733f5e9852279d461115df8376a7ed1160db8ed
guoyukai <643582433@qq.com>
2022-06-29 16:41:14 +0800

    mybatis + aop + mysql 读写分离示例

--- lab-17/lab-17-dynamic-datasource-mybatis/src/main/java/cn/iocoder/springboot/lab17/dynamicdatasource/Application.java
+++ lab-17/lab-17-dynamic-datasource-mybatis/src/main/java/cn/iocoder/springboot/lab17/dynamicdatasource/Application.java
@@ -3,6 +3,12 @@ package cn.iocoder.springboot.lab17.dynamicdatasource;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+/**
+ * mybatis + aop + mysql 读写分离示例，参考：https://mp.weixin.qq.com/s/0YScuegIiFDmh_J-A3iQmQ
+ *
+ * @author Jaquez
+ * @date 2022/06/29 16:39
+ */
 @SpringBootApplication
 @EnableAspectJAutoProxy(exposeProxy = true) // http://www.voidcn.com/article/p-zddcuyii-bpt.html
 public class Application {
--- lab-82-design-mode/lab-82-design-mode-demo-02/src/main/java/cn/iocoder/springboot/lab82/demo/DemoApplication.java
+++ lab-82-design-mode/lab-82-design-mode-demo-02/src/main/java/cn/iocoder/springboot/lab82/demo/DemoApplication.java
@@ -1,7 +1,7 @@
 package cn.iocoder.springboot.lab82.demo;
 
 /**
- * 工程模式演示示例
+ * 工程模式演示示例，应用场景参考：http://www.mydlq.club/article/119/
  *
  * 创建一个或多个相关对象，而使用者不需要关系具体的实现类
  *
