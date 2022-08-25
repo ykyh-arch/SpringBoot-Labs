5d4a15ba93e21d5612619c93f5043af3ce9ddf2d
guoyukai <643582433@qq.com>
2022-08-25 14:10:48 +0800

    springcloud zookeer 入门测试

--- labx-26/labx-26-sc-zookeeper-config-auto-refresh/src/main/java/cn/iocoder/springcloud/labx26/zookeeperdemo/DemoApplication.java
+++ labx-26/labx-26-sc-zookeeper-config-auto-refresh/src/main/java/cn/iocoder/springcloud/labx26/zookeeperdemo/DemoApplication.java
@@ -3,6 +3,12 @@ package cn.iocoder.springcloud.labx26.zookeeperdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ *
+ * @author Jaquez
+ * @date 2022/08/25 14:08
+ */
 @SpringBootApplication
 public class DemoApplication {
 
