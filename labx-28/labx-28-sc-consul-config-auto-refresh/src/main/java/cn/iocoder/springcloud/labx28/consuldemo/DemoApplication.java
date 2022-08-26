aacf722d20f38b3dcac9d5bab22434aea30c44ea
guoyukai <643582433@qq.com>
2022-08-26 15:02:50 +0800

    spring consul 配置中心入门测试

--- labx-28/labx-28-sc-consul-config-auto-refresh/src/main/java/cn/iocoder/springcloud/labx28/consuldemo/DemoApplication.java
+++ labx-28/labx-28-sc-consul-config-auto-refresh/src/main/java/cn/iocoder/springcloud/labx28/consuldemo/DemoApplication.java
@@ -3,6 +3,12 @@ package cn.iocoder.springcloud.labx28.consuldemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ *
+ * @author Jaquez
+ * @date 2022/08/26 14:53
+ */
 @SpringBootApplication
 public class DemoApplication {
 
--- labx-28/labx-28-sc-consul-config-demo/src/main/java/cn/iocoder/springcloud/labx28/consuldemo/DemoApplication.java
+++ labx-28/labx-28-sc-consul-config-demo/src/main/java/cn/iocoder/springcloud/labx28/consuldemo/DemoApplication.java
@@ -3,6 +3,12 @@ package cn.iocoder.springcloud.labx28.consuldemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ *
+ * @author Jaquez
+ * @date 2022/08/26 14:21
+ */
 @SpringBootApplication
 public class DemoApplication {
 
