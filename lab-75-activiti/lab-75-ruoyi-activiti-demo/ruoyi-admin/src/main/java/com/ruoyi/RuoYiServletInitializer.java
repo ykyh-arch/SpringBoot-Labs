37e0f1a95519ce6caa42b6a43d58009ec85baac0
guoyukai <643582433@qq.com>
2023-03-15 10:34:14 +0800

    proguard 代码混淆（未测试通过）

--- lab-75-activiti/lab-75-ruoyi-activiti-demo/ruoyi-admin/src/main/java/com/ruoyi/RuoYiServletInitializer.java
+++ lab-75-activiti/lab-75-ruoyi-activiti-demo/ruoyi-admin/src/main/java/com/ruoyi/RuoYiServletInitializer.java
@@ -4,7 +4,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
- * web容器中进行部署
+ * web容器中进行部署，即使用 war 包进行部署方式，参考：https://mp.weixin.qq.com/s/wdiAlQKxUA05d19e7bBlIw
  * 
  * @author ruoyi
  */
--- lab-80-oss/lab-80-oss-demo-fastdfs/src/main/java/cn/iocoder/springboot/lab80/oss/FileClientApplication.java
+++ lab-80-oss/lab-80-oss-demo-fastdfs/src/main/java/cn/iocoder/springboot/lab80/oss/FileClientApplication.java
@@ -1,8 +1,13 @@
 package cn.iocoder.springboot.lab80.oss;
 
 import com.github.tobato.fastdfs.FdfsClientConfig;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanNameGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.context.annotation.Import;
 import org.springframework.jmx.support.RegistrationPolicy;
@@ -10,8 +15,7 @@ import org.springframework.jmx.support.RegistrationPolicy;
 /**
  * 入口类
  * fastdfs 安装参考：https://hub.docker.com/r/qbanxiaoli/fastdfs
- * 案例参考：https://mp.weixin.qq.com/s/n_fxKVC3zc-6-WM6tNFECw
- * 参考：https://github.com/tobato/FastDFS_Client
+ * 案例参考：https://mp.weixin.qq.com/s/n_fxKVC3zc-6-WM6tNFECw,https://github.com/tobato/FastDFS_Client
  *
  * @author Jaquez
  * @date 2021/12/08 17:05
@@ -21,8 +25,14 @@ import org.springframework.jmx.support.RegistrationPolicy;
 @SpringBootApplication
 public class FileClientApplication {
 
+//    public static void mainMaster(String[] args) {
+//        SpringApplication.run(FileClientApplication.class, args);
+//    }
+
     public static void main(String[] args) {
-        SpringApplication.run(FileClientApplication.class, args);
+        new SpringApplicationBuilder(FileClientApplication.class)
+                .beanNameGenerator((beanDefinition, beanDefinitionRegistry) -> beanDefinition.getBeanClassName())
+                .run(args);
     }
 
 }
