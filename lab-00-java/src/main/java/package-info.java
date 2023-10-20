6a4ffaf40c2d3934202cedb1bc6755094c398c8f
guoyukai <643582433@qq.com>
2023-10-20 15:12:08 +0800

    java 模块包添加package-info

---
+++ lab-00-java/src/main/java/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 1、并发编程参考：https://mp.weixin.qq.com/mp/appmsgalbum?__biz=MzA5MTkxMDQ4MQ==&action=getalbum&album_id=1318984626890915841&scene=173&from_msgid=2648933019&from_itemidx=1&count=3&nolastread=1#wechat_redirect
+ *
+ */
